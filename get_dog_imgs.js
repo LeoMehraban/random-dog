3b0479e1f56b7073fea3e29c6a644608fa9e549b
LeoMehraban <62709564+LeoMehraban@users.noreply.github.com>
2020-05-06 16:01:44 -0400

    my code will Not work with the dog.ceo api

--- get_dog_imgs.js
+++ get_dog_imgs.js
@@ -1,26 +1,14 @@
-             // this will get the dog imgs
-            let img2;
-            let imgs;
+
             async function getDogImg(){
-//                 const responce = await fetch('https://random.dog/woof.json')
-//                 const json = await responce.json()
+                const responce = await fetch('https://random.dog/woof.json')
+                const json = await responce.json()
                 const responce1 = await fetch('https://dog.ceo/api/breeds/image/random')
                 const json1 = await responce1.json()
                 console.log("i got the img!!!!!!!!!");
-//                 if(checkURL(json.url)){
-//                     imgs.push(loadImage(json.url));
-//                     ok = true
-//                 }else{
-//                     ok = false
-//                 }
-                if(checkURL(json1.url)){
-                    imgs.push(loadImage(json1.url));
+                if(checkURL(json.url)){
+                    img1 = loadImage(json.url);
                     ok = true
-                    img1 = floor(random(imgs.length));
                 }else{
                     ok = false
                 }
-                
-                
-                //return img1
             }
