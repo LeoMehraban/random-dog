e96df2214c0b4e54bf9d85260d011522b98e9fb9
LeoMehraban <62709564+LeoMehraban@users.noreply.github.com>
2020-05-06 08:30:42 -0400

    Update dog.js

--- dog.js
+++ dog.js
@@ -1,1 +1,19 @@
+            function checkURL(url) {
+                return(url.match(/\.(jpeg|jpg|gif|png)$/) != null);
+            }
+            function setup(){
+                createCanvas(windowWidth,windowHeight)
+                getDogImg()
+                setInterval(() => {
+                    getDogImg()
+                },1000)
+            }
+            function draw(){
+                //translate(width/2,height/2)
+                imageMode(CENTER);
+                //background(225,0,0)
+                if(ok){
+                    image(img1,width/2,height/2,width,height)
+                }
+            }
 
