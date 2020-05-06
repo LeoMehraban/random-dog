9ddff56484c1f1fd41ca924d03cd5b053398b145
LeoMehraban <leomehraban@icloud.com>
2020-05-06 09:56:10 -0400

    trying out git

--- get_dog_imgs.js
+++ get_dog_imgs.js
@@ -2,6 +2,7 @@
             async function getDogImg(){
                 const responce = await fetch('https://random.dog/woof.json')
                 const json = await responce.json()
+                console.log("i got the img!!!!!!!!!");
                 if(checkURL(json.url)){
                     img1 = loadImage(json.url)
                     ok = true
