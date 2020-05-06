05a6eb3a180b13adfbeb6aae5527cadf7ca7dfa1
LeoMehraban <62709564+LeoMehraban@users.noreply.github.com>
2020-05-06 15:54:19 -0400

    using 2 apis!!!!! 🐶!

--- get_dog_imgs.js
+++ get_dog_imgs.js
@@ -1,14 +1,26 @@
              // this will get the dog imgs
+            let img2;
+            let imgs;
             async function getDogImg(){
                 const responce = await fetch('https://random.dog/woof.json')
                 const json = await responce.json()
+                const responce1 = await fetch('https://dog.ceo/api/breeds/image/random')
+                const json1 = await responce.json()
                 console.log("i got the img!!!!!!!!!");
                 if(checkURL(json.url)){
-                    img1 = loadImage(json.url)
+                    imgs.push(loadImage(json.url));
                     ok = true
                 }else{
                     ok = false
                 }
+                if(checkURL(json1.url)){
+                    imgs.push(loadImage(json1.url));
+                    ok = true
+                    img1 = floor(random(imgs.length));
+                }else{
+                    ok = false
+                }
+                
                 
                 //return img1
             }
