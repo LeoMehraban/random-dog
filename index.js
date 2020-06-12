a42edbd437e9d22f2dc7240a42d08b58e3af89cb
LeoMehraban <leomehraban@icloud.com>
2020-06-12 11:49:46 -0400

    adding electron support

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const electron = require('electron');
 const url = require('url');
 const path = require('path');
 const {app,BrowserWindow} = electron;
+const mainWindow;
 app.on('ready',() => {
     mainWindow = new BrowserWindow({});
     mainWindow.loadURL(url.format({
